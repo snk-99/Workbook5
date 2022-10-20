7e9fb352e43ac6a0f325be4fb620cd4b8269beca
Samuel Kahura <kahurasamuel@gmail.com>
2022-10-20 14:47:07 -0400

    lottery tickets setup

---
+++ Exercises/LotteryTickets/scripts/main.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let winningTickets = [
+    { tixNum: "1001001", expires: "2022-09-05", prize: 100000 },
+    { tixNum: "1298711", expires: "2022-10-10", prize: 250000 },
+    { tixNum: "1309182", expires: "2022-12-30", prize: 500000 },
+    { tixNum: "1456171", expires: "2023-01-20", prize: 1000000 },
+    { tixNum: "3332871", expires: "2022-05-20", prize: 1000000 },
+    { tixNum: "4651529", expires: "2022-12-15", prize: 100000 },
+    { tixNum: "5019181", expires: "2023-01-31", prize: 250000 },
+    { tixNum: "5168261", expires: "2023-03-01", prize: 1000000 },
+    { tixNum: "6761529", expires: "2022-12-15", prize: 250000 },
+    { tixNum: "7778172", expires: "2023-01-15", prize: 5000000 },
+    { tixNum: "8751426", expires: "2020-09-15", prize: 100000 }
+];
+
+window.onload = () => {
+    loadWinningTicketsTable()
+};
+
+function loadWinningTicketsTable() {
+
+}
